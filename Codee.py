92446ff289b4e8d20a5596d9efa84fd5e8fbe291
ArkadeepK <ArkadeepK@users.noreply.github.com>
2023-06-28 23:05:02 +0530

    Update Codee.py

--- Codee.py
+++ Codee.py
@@ -4,3 +4,6 @@ print("Hello World")
 password = "Secret123"
 aws_access_key_id     = AKIAUUSSMNCWIOMASVHG
 aws_secret_access_key = ltU4LMKyCSePHGgxYfhPQlU7nbQbJalkduez9MsN9
+
+aws_access_key_id     = AKIAUUSSMNCWIOMASVHG
+aws_secret_access_key = ltU4LMKyCSePHGgxYfhPQlU7nbQbJalkduez9MsN9
